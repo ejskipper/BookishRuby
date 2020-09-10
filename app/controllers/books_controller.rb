337b30f3461a3d2cf91eaf4eb96e3d6898fb7167
ejskipper <ellen.skipper@softwire.com>
2020-09-10 11:42:23 +0100

    Facilitate adding books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,17 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
   end
+  def create
+    @book = Book.new(book_params)
+     if @book.save
+        flash[:success] = "New Book created."
+        redirect_to '/books/index'
+      else
+        render 'new'
+      end
+  end
+  private
+  def book_params
+    params.require(:book).permit(:title, :author, :isbn)
+  end
 end
