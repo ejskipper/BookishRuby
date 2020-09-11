0313351f628c0f52e0f701af267b658b8f5aaf94
ejskipper <ellen.skipper@softwire.com>
2020-09-11 11:16:52 +0100

    Add/edit/delete copies, alphabetise, links etc

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,37 +1,45 @@
 class BooksController < ApplicationController
   def index
-    @books = Book.all
+    @books = Book.all.sort_by(&:title)
   end
+
   def show
     @book = Book.find(params[:id])
+    @copies = Book.find(params[:id]).copies
   end
+
   def edit
     @book = Book.find(params[:id])
   end
+
   def update
     @book = Book.find(params[:id])
     if @book.update_attributes(book_params)
       flash[:success] = "Book edited."
       redirect_to books_path
     else
-      render 'edit'
+      render "edit"
     end
   end
+
   def destroy
     Book.find(params[:id]).destroy
     flash[:success] = "Book deleted"
     redirect_to books_path
   end
+
   def create
     @book = Book.new(book_params)
-     if @book.save
-        flash[:success] = "New Book created."
-        redirect_to books_path
-      else
-        render 'new'
-      end
+    if @book.save
+      flash[:success] = "New Book created."
+      redirect_to books_path
+    else
+      render "new"
+    end
   end
+
   private
+
   def book_params
     params.require(:book).permit(:title, :author, :isbn)
   end
